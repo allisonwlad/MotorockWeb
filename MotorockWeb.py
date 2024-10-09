76e018d70d29b0e79a19a3661c2f7f835db9cfca
Allison Wladmir <allisonwlad@gmail.com>
2024-10-09 11:48:02 -0300

    add images and icons

--- MotorockWeb.py
+++ MotorockWeb.py
@@ -1,1 +1,15 @@
 import streamlit as st
+
+st.set_page_config(page_title="Motorock", layout="wide", page_icon="https://lh5.googleusercontent.com/-1I_gEnK6xa4/AAAAAAAAAAI/AAAAAAAAAAA/m20AONrAkz4/s40-c-k-mo/photo.jpg")
+
+st.markdown("""
+    <style>
+        .st-emotion-cache-yfhhig {
+            visibility: hidden;
+        }
+        .st-emotion-cache-mnu3yk {
+            visibility: hidden
+        } 
+        
+    </style>
+""", unsafe_allow_html=True)
